1558264c0845a4b148efcdc3a635d105c05960d4
user <user@user.user>
2020-10-24 07:35:39 +0000

    refactor emitter

---
+++ src/emitter.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+type emitter struct {
+	rMap map[string]int
+	creg int
+}
+
+func (e *emitter) init() {
+	e.rMap = make(map[string]int)
+	e.creg = 1
+}
+
+func (e *emitter) err(msg string) {
+
+	panic(fmt.Sprintln(msg, e.rMap, e.creg))
+}
+
+func (e *emitter) emitExpr(ex Expr) string {
+	rt := ""
+	switch t := ex.(type) {
+	case NumberExpr:
+		rt += t.Il.Value
+	case VarExpr:
+		rt += "w" + fmt.Sprint(e.rMap[t.Wl.Value])
+	case BinaryExpr:
+		e.err("")
+	}
+
+	return rt
+}
+
+func (e *emitter) emitStmt(s Stmt) string {
+	rt := ""
+	switch t := s.(type) {
+	case ReturnStmt:
+		rt += "  mov w0, "
+		rt += e.emitExpr(t.E) + "\n"
+		rt += "  ret\n"
+	case AssignStmt:
+		lh := e.emitExpr(t.LHSa[0])
+		rh := ""
+		switch t2 := t.RHSa[0].(type) {
+		case NumberExpr, VarExpr:
+			rh += e.emitExpr(t2)
+			rt += "  mov " + lh + ", " + rh + "\n"
+			return rt
+		case BinaryExpr:
+			rt = "  add " + lh + ", " + e.emitExpr(t2.LHS) + ", " + e.emitExpr(t2.RHS) + "\n"
+			return rt
+		}
+		//rh := e.emitExpr(t.RHSa[0])
+
+
+	case VarStmt:
+		s := t.List[0].Value
+		e.rMap[s] = e.creg
+		e.creg++
+	}
+	return rt
+
+}
+
+func (e *emitter) emit(f File) string {
+	rt := `
+.global main
+main:
+`
+	for _, s := range f.SList {
+		rt += e.emitStmt(s)
+	}
+	//	rt += "ret\n"
+	return rt
+}
