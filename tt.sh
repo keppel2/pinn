32499d89468350cffd70237e13b969e31a8e47e9
Ryan Keppel <ryankeppel@M-2.local>
2021-01-07 21:01:59 -0800

    ci

--- tt.sh
+++ tt.sh
@@ -1,1 +1,1 @@
-./pp.sh pparse && for i in {1..5}; do ./mt.sh em$i || exit 1 ; done  && ./pr.sh pr1
+./pp.sh pparse && for i in {1..5}; do ./mt.sh em$i || exit 1 ; done  && ./pr.sh pr1 && rm *.S *.out pr1.txt
