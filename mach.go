545bc262c53d80925c8c1fd06bd9e345a79a7f36
Ryan Keppel <ryankeppel@M-2.local>
2021-02-21 15:48:39 -0800

    mach

--- mach.go
+++ mach.go
@@ -4,8 +4,8 @@ import "debug/macho"
 import "fmt"
 
 func main() {
-  f, _ := macho.Open("m.out")
+  f, _ := macho.Open("si.outx")
   ss, _ := f.ImportedLibraries()
   fmt.Println(ss)
-  fmt.Printf("%+v\n", f)
+  fmt.Printf("%#v\n", f)
 }
